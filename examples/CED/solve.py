fe33b9f0d8bfdcd73d48554b0a483c5d09a78cd2
wang_yu <1372563282@qq.com>
2018-10-29 09:28:32 +0800

    update 20181025

---
+++ examples/CED/solve.py
@@ -0,0 +1,18 @@
+from __future__ import division
+import numpy as np
+import sys
+caffe_root = '/path/to/caffe_root' 
+sys.path.insert(0, caffe_root + '/python')
+import caffe
+
+
+base_weights = './hed_vgg16.caffemodel'
+
+# init
+caffe.set_mode_gpu()
+caffe.set_device()
+
+solver = caffe.SGDSolver('solver.prototxt')
+
+# copy base weights for fine-tuning
+solver.net.copy_from(base_weights)
